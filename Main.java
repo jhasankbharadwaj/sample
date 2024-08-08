7e1a2c76ea01eb4b37ca4abf5c3e4af029fad6b5
jhasankbharadwaj <218r5a6711@gmail.com>
2024-08-08 20:34:29 +0530

    first branch

--- Main.java
+++ Main.java
@@ -1,6 +1,11 @@
+
 public class Main{
+    public void fun1(){
+System.out.println("this is function one written in s1 branch ");
+    }
     public static void main(String args[]){
          System.out.println("this main class was made in main");
-         
+         Main obj=new Main();
+         obj.fun1();
     }
 }
