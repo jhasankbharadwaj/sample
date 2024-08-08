678c08417b3641ab6e681ec5c506944ec4568355
jhasankbharadwaj <218r5a6711@gmail.com>
2024-08-08 22:10:15 +0530

    for revert

--- Main.java
+++ Main.java
@@ -6,6 +6,43 @@ System.out.println("this is function one written in s1 branch  once again ");
 System.out.println("this is function one written in s1 branch  once again ");
 
     }
+    public void fun2(){
+        System.out.println("this is function one written in s1 branch  once again ");
+        System.out.println("this is function one written in s1 branch  once again ");
+        System.out.println("this is function one written in s1 branch  once again ");
+        
+            } 
+        public void fun3(){
+                System.out.println("this is function one written in s1 branch  once again ");
+                System.out.println("this is function one written in s1 branch  once again ");
+                System.out.println("this is function one written in s1 branch  once again ");
+                
+                    }
+ public void fun2(){
+                        System.out.println("this is function one written in s1 branch  once again ");
+                        System.out.println("this is function one written in s1 branch  once again ");
+                        System.out.println("this is function one written in s1 branch  once again ");
+                        
+                            } 
+     public void fun3(){
+                                System.out.println("this is function one written in s1 branch  once again ");
+                                System.out.println("this is function one written in s1 branch  once again ");
+                                System.out.println("this is function one written in s1 branch  once again ");
+                                
+                                    }
+     public void fun4(){
+        System.out.println("this is function one written in s1 branch  once again ");
+        System.out.println("this is function one written in s1 branch  once again ");
+        System.out.println("this is function one written in s1 branch  once again ");
+        
+            } 
+        public void fun33(){
+                System.out.println("this is function one written in s1 branch  once again ");
+                System.out.println("this is function one written in s1 branch  once again ");
+                System.out.println("this is function one written in s1 branch  once again ");
+                
+                    }
+                
     public static void main(String args[]){
          System.out.println("this main class was made in main");
          Main obj=new Main();
